37f7ff72f16241ed0552e6a3d5b24f4b77f51b5e
Nathan Alleyne <32268513+nate8all@users.noreply.github.com>
2020-04-13 18:31:48 -0400

    read/write to and from firebase
    Read new database data and display changes on the screen including user IDs and their status.

--- src/games/wheeloffortune/WheelofFortune.js
+++ src/games/wheeloffortune/WheelofFortune.js
@@ -1,19 +1,59 @@
 import GameComponent from "../../GameComponent.js";
+import UserApi from "../../UserApi.js";
 import React from "react";
 
 export default class WheelofFortune extends GameComponent {
+  constructor(props) {
+    super(props);
+    this.getSessionDatabaseRef().set({ text: "Hello, World!" });
+    this.state = { last_user_id: null };
+  }
+
+  onSessionDataChanged(data) {
+    console.log("Data changed", data);
+    console.log(this.state.last_user_id + "");
+    this.setState({ last_user_id: data.user_id });
+    console.log(this.state.last_user_id + "");
+  }
+
+  handleButtonClick() {
+    this.getSessionDatabaseRef().set({ user_id: this.getMyUserId() });
+  }
+
   render() {
     var id = this.getSessionId();
     var users = this.getSessionUserIds().map(user_id => (
-      <li key={user_id}>{user_id}</li>
+      <li key={user_id}>{UserApi.getName(user_id)}</li>
     ));
-    var creator = this.getSessionCreatorUserId();
+    var creator = UserApi.getName(this.getSessionCreatorUserId());
+    var title = this.getSessionTitle();
+    var me = UserApi.getName(this.getMyUserId());
+
+    var status = "";
+    if (this.getMyUserId() === this.getSessionCreatorUserId()) {
+      // Display "I am a host"
+      status = "I am a host";
+    } else {
+      // Dipsplay " I am a guest"
+      status = "I am a guest";
+    }
+
+    var last_user = "No one";
+    if (this.state.last_user_id != null) {
+      last_user = UserApi.getName(this.state.last_user_id);
+    }
+    var last_user_message = last_user + " clicked the button!";
     return (
       <div>
         <p>Session ID: {id}</p>
+        <p>Session Title: {title}</p>
         <p>Session Creator: {creator} </p>
+        <p>Me: {me}</p>
+        <p>Status: {status}</p>
         <p>Session users: </p>
         <ul>{users}</ul>
+        <button onClick={() => this.handleButtonClick()}>Click me!</button>
+        <p>{last_user_message}</p>
       </div>
     );
   }
