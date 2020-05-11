843a73575aee0dce8d342fb37af29cc02f28018d
Nathan Alleyne <32268513+nate8all@users.noreply.github.com>
2020-05-11 18:23:23 -0400

    05/11 Commit
    Added question and player index to state, updated layout for game for when switching from player to player

--- src/games/wheeloffortune/WheelofFortune.js
+++ src/games/wheeloffortune/WheelofFortune.js
@@ -6,56 +6,63 @@ import Question from "./Question.js";
 export default class WheelofFortune extends GameComponent {
   constructor(props) {
     super(props);
-    this.getSessionDatabaseRef().set({ text: "Hello, World!" });
+    // this.getSessionDatabaseRef().set({ text: "Hello, World!" });
     this.state = {
       last_user_id: null,
-      question: "What time is it",
+      question: [
+        "What time is it",
+        "What's your favorite color",
+        "When is your birthday"
+      ],
       answers: ["1", "2", "3", "4"],
       response: "[No response yet]",
-      user_ids: []
+      user_ids: [],
+      player_index: 0,
+      question_index: 0
       // whose turn it is
       // points
       // timer (v2)
       // question difficulty
       // correct answer
     };
-    this.getPlayers();
   }
 
   onSessionDataChanged(data) {
     console.log("Data changed", data);
     console.log(this.state.last_user_id + "");
-    this.setState({
+    console.log("player index before " + this.state.player_index);
+    this.setState(prevState => ({
       last_user_id: data.user_id,
       response: data.response,
-      user_ids: data.user_ids
-    });
-    console.log(this.state.last_user_id + "");
-  }
-
-  handleButtonClick() {
-    this.getSessionDatabaseRef().set({ user_id: this.getMyUserId() });
+      player_index: data.player_index
+    }));
+    console.log("player index after " + this.state.player_index);
+    console.log("STATE RESPONSE " + this.state.response);
   }
 
   handleSubmitButton() {
     console.log(document.getElementById("response").value);
-    this.getSessionDatabaseRef().set({
-      user_id: this.getMyUserId(),
-      response: document.getElementById("response").value
-    });
-  }
+    console.log("handle submit button index before " + this.state.player_index);
+    var new_index = this.state.player_index;
+    if (this.state.player_index === 2) {
+      new_index = 0;
+    } else {
+      new_index = this.state.player_index + 1;
+    }
 
-  getPlayers() {
-    this.getSessionDatabaseRef().set({
-      user_ids: this.getSessionUserIds()
+    if (this.state.question_index === this.state.question.length - 1) {
+      // insert stuff in here next time!
+    }
+
+    this.getSessionDatabaseRef().update({
+      user_id: this.getMyUserId(),
+      response: document.getElementById("response").value,
+      player_index: new_index
     });
+    console.log("handle submit button index after " + this.state.player_index);
   }
 
   render() {
-    if (this.getSessionUserIds().length === 3) {
-      this.getPlayers();
-    }
-
     var id = this.getSessionId();
     var users = this.getSessionUserIds().map(user_id => (
       <li key={user_id}>{UserApi.getName(user_id)}</li>
@@ -77,22 +84,22 @@ export default class WheelofFortune extends GameComponent {
     if (this.state.last_user_id != null) {
       last_user = UserApi.getName(this.state.last_user_id);
     }
-    if (this.state.user_ids != null) {
-      var user_ids = this.state.user_ids.map(user_id => (
-        <li key={user_id}>{UserApi.getName(user_id)}</li>
-      ));
-      var first_player = UserApi.getName(this.state.user_ids[0]);
-      var player_index = 0;
-      // var player_index_max = this.state.user_ids.length;
 
-      if (this.state.user_ids[player_index] === this.getMyUserId()) {
-        var input_text_box = <input type="text" id="response" />;
-        var submit_button = (
-          <button onClick={() => this.handleSubmitButton()}> Submit </button>
-        );
-      }
+    // Using get Session Id here to get all players
+    var user_ids = this.getSessionUserIds().map(user_id => (
+      <li key={user_id}>{UserApi.getName(user_id)}</li>
+    ));
+    var player_index = this.state.player_index;
+    var current_player = UserApi.getName(
+      this.getSessionUserIds()[player_index]
+    );
+
+    if (this.getSessionUserIds()[player_index] === this.getMyUserId()) {
+      var input_text_box = <input type="text" id="response" />;
+      var submit_button = (
+        <button onClick={() => this.handleSubmitButton()}> Submit </button>
+      );
     }
-    var last_user_message = last_user + " clicked the button!";
     var last_user_with_response =
       last_user + " responded with " + this.state.response;
 
@@ -105,15 +112,17 @@ export default class WheelofFortune extends GameComponent {
         <p>Status: {status}</p>
         <p>Session users: </p>
         <ul>{users}</ul>
-        <button onClick={() => this.handleButtonClick()}>Click me!</button>
-        <p>{last_user_message}</p>
-        <Question question={this.state.question} answers={this.state.answers} />
+        <Question
+          question={this.state.question[0]}
+          answers={this.state.answers}
+        />
         <p>
           {input_text_box} {submit_button}
         </p>
         <p> {last_user_with_response} </p>
+        <p> {this.state.response} </p>
         <ul> {user_ids} </ul>
-        <p> {first_player} </p>
+        <p> {current_player} </p>
       </div>
     );
   }
