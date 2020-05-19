1f29853a3776cbb43ace0c6d108334df71f24d34
nthav <nthav@users.noreply.github.com>
2020-05-19 10:38:28 -0400

    adding wheel and css
    adding wheel and css

---
+++ src/games/wheeloffortune/Data.js
@@ -0,0 +1,9 @@
+let question_data = [
+  { question: "What time is it?", answer: "17" },
+  { question: "What time is it 2?", answer: "17" },
+  { question: "What time is it 3?", answer: "17" },
+  { question: "What time is it 4?", answer: "17" },
+  { question: "What time is it 5?", answer: "17" }
+];
+
+export default question_data;
--- src/games/wheeloffortune/WheelofFortune.js
+++ src/games/wheeloffortune/WheelofFortune.js
@@ -2,60 +2,66 @@ import GameComponent from "../../GameComponent.js";
 import UserApi from "../../UserApi.js";
 import React from "react";
 import Question from "./Question.js";
+import Wheel from "./SpinningWheel.js";
+import question_data from "./Data.js";
+import "./WheelofFortune.css";
 
 export default class WheelofFortune extends GameComponent {
   constructor(props) {
     super(props);
-    this.getSessionDatabaseRef().set({ text: "Hello, World!" });
+    // this.getSessionDatabaseRef().set({ text: "Hello, World!" });
     this.state = {
       last_user_id: null,
-      question: "What time is it",
+      question: question_data,
       answers: ["1", "2", "3", "4"],
       response: "[No response yet]",
-      user_ids: []
+      user_ids: [],
+      player_index: 0,
+      question_index: 0
       // whose turn it is
       // points
       // timer (v2)
       // question difficulty
       // correct answer
     };
-    this.getPlayers();
   }
 
   onSessionDataChanged(data) {
     console.log("Data changed", data);
     console.log(this.state.last_user_id + "");
-    this.setState({
+    console.log("player index before " + this.state.player_index);
+    this.setState(prevState => ({
       last_user_id: data.user_id,
       response: data.response,
-      user_ids: data.user_ids
-    });
-    console.log(this.state.last_user_id + "");
-  }
-
-  handleButtonClick() {
-    this.getSessionDatabaseRef().set({ user_id: this.getMyUserId() });
+      player_index: data.player_index
+    }));
+    console.log("player index after " + this.state.player_index);
+    console.log("STATE RESPONSE " + this.state.response);
   }
 
   handleSubmitButton() {
     console.log(document.getElementById("response").value);
-    this.getSessionDatabaseRef().set({
-      user_id: this.getMyUserId(),
-      response: document.getElementById("response").value
-    });
-  }
+    console.log("handle submit button index before " + this.state.player_index);
+    var new_index = this.state.player_index;
+    if (this.state.player_index === 2) {
+      new_index = 0;
+    } else {
+      new_index = this.state.player_index + 1;
+    }
 
-  getPlayers() {
-    this.getSessionDatabaseRef().set({
-      user_ids: this.getSessionUserIds()
+    if (this.state.question_index === this.state.question.length - 1) {
+      // insert stuff in here next time!
+    }
+
+    this.getSessionDatabaseRef().update({
+      user_id: this.getMyUserId(),
+      response: document.getElementById("response").value,
+      player_index: new_index
     });
+    console.log("handle submit button index after " + this.state.player_index);
   }
 
   render() {
-    if (this.getSessionUserIds().length === 3) {
-      this.getPlayers();
-    }
-
     var id = this.getSessionId();
     var users = this.getSessionUserIds().map(user_id => (
       <li key={user_id}>{UserApi.getName(user_id)}</li>
@@ -77,43 +83,51 @@ export default class WheelofFortune extends GameComponent {
     if (this.state.last_user_id != null) {
       last_user = UserApi.getName(this.state.last_user_id);
     }
-    if (this.state.user_ids != null) {
-      var user_ids = this.state.user_ids.map(user_id => (
-        <li key={user_id}>{UserApi.getName(user_id)}</li>
-      ));
-      var first_player = UserApi.getName(this.state.user_ids[0]);
-      var player_index = 0;
-      // var player_index_max = this.state.user_ids.length;
 
-      if (this.state.user_ids[player_index] === this.getMyUserId()) {
-        var input_text_box = <input type="text" id="response" />;
-        var submit_button = (
-          <button onClick={() => this.handleSubmitButton()}> Submit </button>
-        );
-      }
-    }
-    var last_user_message = last_user + " clicked the button!";
+    // Using get Session Id here to get all players
+    var user_ids = this.getSessionUserIds().map(user_id => (
+      <li key={user_id}>{UserApi.getName(user_id)}</li>
+    ));
+    var player_index = this.state.player_index;
+    var current_player = UserApi.getName(
+      this.getSessionUserIds()[player_index]
+    );
+    var player_turn = "It's " + current_player + "'s turn";
+
+    var show_text_box =
+      this.getSessionUserIds()[player_index] === this.getMyUserId();
+
+    // if (show_text_box) {
+    //   var input_text_box = <input type="text" id="response" />;
+    //   var submit_button = (
+    //     <button onClick={() => this.handleSubmitButton()}> Submit </button>
+    //   );
+    // }
     var last_user_with_response =
       last_user + " responded with " + this.state.response;
 
     return (
-      <div>
-        <p>Session ID: {id}</p>
+      <div className="div">
+        {/* <p>Session ID: {id}</p>
         <p>Session Title: {title}</p>
-        <p>Session Creator: {creator} </p>
-        <p>Me: {me}</p>
-        <p>Status: {status}</p>
+        <p>Session Creator: {creator} </p> */}
+        {/* <p>Me: {me}</p> */}
+        {/* <p>Status: {status}</p> */}
         <p>Session users: </p>
         <ul>{users}</ul>
-        <button onClick={() => this.handleButtonClick()}>Click me!</button>
-        <p>{last_user_message}</p>
-        <Question question={this.state.question} answers={this.state.answers} />
-        <p>
+        {/* <ul> {user_ids} </ul> */}
+        <p> {player_turn} </p>
+        <Wheel
+          items={this.state.question}
+          show_text_box={show_text_box}
+          player_index={player_index}
+        />
+        {/* <p>
+          Enter your answer here:
           {input_text_box} {submit_button}
-        </p>
+        </p> */}
         <p> {last_user_with_response} </p>
-        <ul> {user_ids} </ul>
-        <p> {first_player} </p>
+        {/* <p> {this.state.response} </p> */}
       </div>
     );
   }
